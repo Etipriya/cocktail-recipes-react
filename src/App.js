05d225e016a82f6bbaf1ed5a531a2371ffe73331
Eti Priya <etipriya@gmail.com>
2023-07-13 02:09:34 +0100

    added code related to api, heading and a searchbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useCallback, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
 
 function App() {
+  const [loading, setloading] = useState(false);
+  const [data, setData] = useState([]);
+
+  const url = "https://www.thecocktaildb.com/api/json/v1/1/random.php";
+
+  const fetchCocktailHandler = useCallback(() => {
+    setloading(true);
+
+    axios
+      .get(url)
+      .then(res => {
+        console.log(res.data);
+        setData(res.data.drinks);
+      })
+      .catch(e => console.log(e))
+      .finally(() => setloading(false));
+  }, []);
+
+  useEffect(() => {
+    fetchCocktailHandler();
+  }, [fetchCocktailHandler]);
+
+  if (loading) {
+    return <h2>Loading</h2>;
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {data.map(cocktail => (
+        <div key={cocktail.idDrink} className="cocktail-container">
+          <h2>{cocktail.strDrink}</h2>
+          <img src={cocktail.strDrinkThumb} alt="#" />
+          <button onClick={fetchCocktailHandler}>
+            What are you looking for?
+          </button>
+        </div>
+      ))}
     </div>
   );
 }
